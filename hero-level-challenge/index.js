7e15a46b7c8f34f7f66ad080414c65d8c168c1c3
Junior Dantas <116525934+juniord-dev@users.noreply.github.com>
2023-10-17 11:20:09 -0300

    desafio finalizado

---
+++ hero-level-challenge/index.js
@@ -0,0 +1,23 @@
+let xp = 100
+let level = ""
+let nome = "Hero"
+
+if (xp <= 1000) {
+  level = "Ferro"
+} else if (xp <= 2000) {
+  level = "Bronze"
+} else if (xp <= 5000) {
+  level = "Prata"
+} else if (xp <= 7000) {
+  level = "Ouro"
+} else if (xp <= 8000) {
+  level = "Platina"
+} else if (xp === 9000) {
+  level = "Ascendente"
+} else if (xp <= 10000) {
+  level = "Imortal"
+} else {
+  level = "Radiante"
+}
+
+console.log("O Herói de nome " + nome + " está no nível de " + level)
